a636355a5394bc7ba4fff90563e53266473aac98
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-02-25 21:40:10 +0100

    Task3

--- src/Server.cs
+++ src/Server.cs
@@ -5,9 +5,16 @@ TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
 
 Socket clientSocket = server.AcceptSocket();
-while (clientSocket.Connected)
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+HandleConnection(clientSocket);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+async Task HandleConnection(Socket socket)
 {
-    Span<byte> buffer = new byte[1024];
-    clientSocket.Receive(buffer);
-    clientSocket.Send("+PONG\r\n"u8.ToArray());
+    while (clientSocket.Connected)
+    {
+        var buffer = new byte[1024];
+        await clientSocket.ReceiveAsync(buffer);
+        await clientSocket.SendAsync("+PONG\r\n"u8.ToArray());
+    }
 }
