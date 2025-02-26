9c4302c1c6a2c5a443adbe33973b5b877015920d
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-02-26 19:15:23 +0100

    Task3Fix3

--- src/Server.cs
+++ src/Server.cs
@@ -4,10 +4,13 @@ using System.Net.Sockets;
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
 
-Socket clientSocket = server.AcceptSocket();
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-HandleConnection(clientSocket);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+while (true)
+{
+    Socket clientSocket = server.AcceptSocket();
+    #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+    HandleConnection(clientSocket);
+    #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+}
 
 async Task HandleConnection(Socket socket)
 {
